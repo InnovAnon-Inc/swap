af555fd204a14bcc287e5b767815773b9c8d0d8f
Default User <laurence.a.maddox@gmail.com>
2017-09-28 03:36:51 -0500

    initial commit

--- src/swap.h
+++ src/swap.h
@@ -9,76 +9,21 @@ extern "C" {
 
 #include <glitter.h>
 
-typedef struct {
-   void *restrict data;
-   size_t esz;
-   size_t n;
-} array_t;
+void swap (void *restrict a, void *restrict b,
+	void *restrict tmp, size_t esz)
+__attribute__ ((leaf, nonnull (1, 2, 3), nothrow)) ;
 
-void *index_array (array_t const *restrict array, size_t i)
-__attribute__ ((leaf, nonnull (1), pure, returns_nonnull, warn_unused_result)) ;
+void swaps (void *restrict a, void *restrict b,
+	void *restrict tmp, size_t esz, size_t n)
+__attribute__ ((nonnull (1, 2, 3), nothrow)) ;
 
-void init_array (array_t *restrict array,
-	void *restrict data, size_t esz, size_t n)
+/* src and dest should not overlap ? */
+void swap2 (void *restrict a, void *restrict b, size_t esz)
 __attribute__ ((leaf, nonnull (1, 2), nothrow)) ;
 
-int alloc_array (array_t *restrict array,
-	size_t esz, size_t n)
-__attribute__ ((nonnull (1), nothrow, warn_unused_result)) ;
-
-int realloc_array (array_t *restrict array, size_t n)
-__attribute__ ((nonnull (1), nothrow, warn_unused_result)) ;
-
-void get_array (array_t const *restrict array, size_t i,
-   void *restrict e)
-__attribute__ ((nonnull (1, 3), nothrow)) ;
-
-void gets_array (array_t const *restrict array, size_t i,
-   void *restrict e, size_t n)
-__attribute__ ((nonnull (1, 3), nothrow)) ;
-
-void set_array (array_t const *restrict array, size_t i,
-	void const *restrict e)
-__attribute__ ((nonnull (1, 3), nothrow)) ;
-
-void sets_array (array_t const *restrict array, size_t i,
-	void const *restrict e, size_t n)
-__attribute__ ((nonnull (1, 3), nothrow)) ;
-
-void free_array (array_t const *restrict array)
-__attribute__ ((leaf, nonnull (1), nothrow)) ;
-
-void cp_array (array_t const *restrict array, size_t i, size_t j)
-__attribute__ ((nonnull (1), nothrow)) ;
-
-/* src and dest should not overlap */
-void cps_array (array_t const *restrict array,
-	size_t i, size_t j, size_t n)
-__attribute__ ((nonnull (1), nothrow)) ;
-
-void mvs_array (array_t const *restrict array,
-	size_t i, size_t j, size_t n)
-__attribute__ ((nonnull (1), nothrow)) ;
-
-/* http://www.cs.utsa.edu/~wagner/CS2213/swap/swap.html */
-
-void swap_array (array_t const *restrict array,
-	size_t i, size_t j, void *restrict tmp)
-__attribute__ ((nonnull (1, 4), nothrow)) ;
-
-void swaps_array (array_t const *restrict array,
-	size_t i, size_t j, size_t n, void *restrict tmp)
-__attribute__ ((nonnull (1, 5), nothrow)) ;
-
 /* src and dest should not overlap ? */
-void swap_array2 (array_t const *restrict array,
-	size_t i, size_t j)
-__attribute__ ((nonnull (1), nothrow)) ;
-
-/* src and dest should not overlap ? */
-void swaps_array2 (array_t const *restrict array,
-	size_t i, size_t j, size_t n)
-__attribute__ ((nonnull (1), nothrow)) ;
+void swaps2 (void *restrict a, void *restrict b, size_t esz, size_t n)
+__attribute__ ((nonnull (1, 2), nothrow)) ;
 
 #ifdef __cplusplus
 }
